aced971e234a33a8a4068fadd0c55cfb32e77fd7
Motaz <motazyusuf@gmail.com>
2025-01-14 16:45:57 +0200

    -Google login finished

--- lib/core/configurations/pages_routes.dart
+++ lib/core/configurations/pages_routes.dart
@@ -3,6 +3,7 @@ class PagesRoutes {
   static const String mainLayout = "/layout";
   static const String memberInformation = "/memberInformation";
   static const String loginWithMail = "/loginMail";
+  static const String loginWithGoogle = "/loginGoogle";
   static const String signUp = "/signUp";
   static const String planSelection = "/planSelection";
   static const String addMember = "/addMember";
